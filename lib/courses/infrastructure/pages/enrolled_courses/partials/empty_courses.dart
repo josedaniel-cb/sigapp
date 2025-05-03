e7c71e5adc7133fb2f0a75a7a6d484e604fa96ac
José Daniel Calle Briceño <j.daniel.c137@gmail.com>
2025-05-03 01:21:07 -0500

    feat: enhance EmptyCoursesWidget to support optional Lottie animation

--- lib/courses/infrastructure/pages/enrolled_courses/partials/empty_courses.dart
+++ lib/courses/infrastructure/pages/enrolled_courses/partials/empty_courses.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class EmptyCoursesWidget extends StatelessWidget {
-  const EmptyCoursesWidget({super.key, this.message});
+  const EmptyCoursesWidget({super.key, this.message, this.lottiePath});
 
   final String? message;
+  final String? lottiePath;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +15,13 @@ class EmptyCoursesWidget extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.info_outline, size: 80, color: Colors.grey),
+          if (lottiePath != null)
+            Lottie.asset(
+              lottiePath!,
+              height: MediaQuery.of(context).size.width * 0.5,
+            )
+          else
+            Icon(Icons.info_outline, size: 80, color: Colors.grey),
           SizedBox(height: 16),
           Text(
             message ?? "No tienes cursos inscritos este semestre",
